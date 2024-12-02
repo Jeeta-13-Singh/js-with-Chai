26746b50e1fbf1cf9182b6623c490aec251d1733
Jeeta-13-Singh <144985327+Jeeta-13-Singh@users.noreply.github.com>
2024-12-02 01:49:51 +0000

    Conversion of datatypes

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Jeeta"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc"=> Nan
+// true =>1; false=>0
+
+
+
+let isLoggedIn = 1
+let isLoggedIn1 = "jeeta"
+let isLoggedIn2 = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn1);
+// console.log(booleanIsLoggedIn2);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+console.log(stringNumber);
+
