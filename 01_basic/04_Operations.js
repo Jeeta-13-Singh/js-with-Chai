130df93c7477b46808b7fe410906eb0b36f6fae6
Jeeta-13-Singh <144985327+Jeeta-13-Singh@users.noreply.github.com>
2024-12-02 02:16:34 +0000

    Operstions On different datatypes

---
+++ 01_basic/04_Operations.js
@@ -0,0 +1,21 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Jeeta"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log("1" +2); //12 concated
+console.log(1 + "2"); //12 concated
+console.log("1" + 2 + 2); //122 concated
+console.log(1 + 2 + "2");// 1+2=3 and 3 concated with "2" as 2 is a string value
+
+console.log((3+4) * 5 % 3);
+
+console.log(+true);//convered into 1(numerical value of true in boolean)
+
+//link to study ecma
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
