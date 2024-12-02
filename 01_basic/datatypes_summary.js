73a13c086333880de2eab7b38ccf5bbdc8cf34b7
Jeeta-13-Singh <144985327+Jeeta-13-Singh@users.noreply.github.com>
2024-12-02 14:58:58 +0000

    Gossip about Strings

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -16,7 +16,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol("123")
 
-console.log(id==anotherId);
+// console.log(id==anotherId);
 const bigNumber = 3456782198706734232n
 
 
@@ -35,3 +35,32 @@ console.log("Hello world");
 }
 
 //https://262.emca-international.orgs/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(used in Primitive), Heap(used in Non_Primitive)
+
+let myYoutubename = "jeetaSingh"
+let anothername = myYoutubename
+anothername = "javaScriptwithchai"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+// In Stack used in Primitive datatypes gives the copy of the value i.e. changes made are done on the copy of the variable so the value only changes in the copy of the variable
+
+let user ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let usernew = user
+
+usernew.email = "jeeta@google.com"
+
+console.log(user.email);
+console.log(usernew.email);
+
+
+// whereas the Heap uses Non-primitive datatypes it gives the reference of the value i.e. changes made are done in the original variable so the value gets changed 
