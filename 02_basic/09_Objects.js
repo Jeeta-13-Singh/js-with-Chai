3913bc4a211d8e4d32d05f83c4547ce242b57159
Jeeta-13-Singh <144985327+Jeeta-13-Singh@users.noreply.github.com>
2024-12-05 03:54:12 +0000

    Timepass with Objects

--- 02_basic/09_Objects.js
+++ 02_basic/09_Objects.js
@@ -18,6 +18,8 @@ const JsUser = {
 // console.log(JsUser["email"]);
 // console.log(JsUser["FullName"]);
 // console.log(typeof JsUser[mysym]);
+// console.log(typeof JsUser["age"]);
+// console.log(typeof JsUser["lastLoginDays"]);
 
 JsUser.email = "Jeeta@chatgpt.com"
 // Object.freeze(JsUser)
@@ -40,7 +42,7 @@ JsUser.greetingTwo = function(){
 
 // singleton
 // Object.create
-
+// When ever we declare an object using a constructor it is of Singleton form
 // const tinderuser = new Object()
 const tinderuser = {}
 
@@ -65,11 +67,52 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = {obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = {obj1, obj2 } //Returns Object within Object
+const obj3 = Object.assign({}, obj1, obj2, obj4)
+ 
 
-const obj3 = {...obj1, ...obj2, ...obj4}
+// const obj3 = {...obj1, ...obj2, ...obj4}
 // console.log(obj3);
 
 
+const users = [
+    {
+        id: 1,
+        email: "j@gmail.com"
+    },
+    {
+        id: 2,
+        email: "e@gmail.com"
+    },
+    {
+        id: 3,
+        email: "t@gmail.com"
+    },
+]
+
+// console.log(users[1].email);
+// console.log(tinderuser);
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+
+// console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+const course = {
+    coursename: "Chai aur Padahi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+const {price: coursePrice} = course
+
+console.log(Instructor);
+console.log(coursePrice);
 
